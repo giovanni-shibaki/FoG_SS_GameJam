d9111d2503bbdcf9fd27203a1fc3ba081ea26004
lucasmihara <miharalucas@gmail.com>
2024-03-25 22:26:00 -0300

    size changes to planets

---
+++ Assets/Code/Scripts/Interfaces/IMassObject.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IMassObject {
+    float Mass { get; set; }
+    float Radius { get; set; }
+}
--- Assets/PlanetMovement.cs
+++ Assets/PlanetMovement.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public enum ResourceType
 {
@@ -12,48 +13,52 @@ public enum ResourceType
 
 public class PlanetMovement : MonoBehaviour
 {
-    public GameObject player; // Reference to the player GameObject
+    public Rigidbody2D planet;
     public float attachDistance = 10f; // Distance at which the object attaches to the player
     public float rotationSpeed = 30f; // Speed of rotation around the player
     public float circleInterval = 3f; // Interval between circle creation (in seconds)
     public GameObject circlePrefab; // Prefab of the circle object
     public ResourceType resourceType;
 
+    private GameObject player; // Reference to the player GameObject
     private bool isAttached = false;
     private Vector3 initialOffset;
     private float circleTimer = 0f;
 
+    void Awake() {
+        planet = GetComponent<Rigidbody2D>();
+        GetComponent<CircleCollider2D>().radius = planet.mass;
+        transform.localScale = Vector3.one * planet.mass;
+    }
+
     void Start()
     {
         // Calculate initial offset from the player
-        initialOffset = transform.position - player.transform.position;
     }
 
     void Update()
     {
-        if (!isAttached && player != null)
-        {
-            // Calculate the distance between this object and the player
-            float distance = Vector3.Distance(transform.position, player.transform.position);
-
-            // Check if the distance is less than the attach distance
-            if (distance <= attachDistance)
-            {
-                AttachToPlayer();
-            }
-        }
-        else if (isAttached)
+        if (isAttached)
         {
             RotateAroundPlayer();
             ManageCircleTimer();
         }
     }
 
-    void AttachToPlayer()
+    private void OnTriggerEnter2D(Collider2D collider) {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            AttachToPlayer(collider.gameObject);
+        }
+    }
+
+    void AttachToPlayer(GameObject player)
     {
         isAttached = true;
+        this.player = player;
         transform.SetParent(player.transform);
         initialOffset = transform.position - player.transform.position; // Update initial offset
+        rotationSpeed *= Random.Range(0, 2) == 0 ? 1 : -1;
     }
 
     void RotateAroundPlayer()
